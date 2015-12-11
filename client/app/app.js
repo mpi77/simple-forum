d944b51b4e0ddb5ed3de4e721c1d2d8a8d21927f
Martin Bureš <m2bures@gmail.com>
2015-12-11 22:27:48 +0100

    added webpack config;

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
+require("jquery");
+require("bootstrap");
+require("angular");
+require("angular-route");
+require("./index/index.js");
+
 angular.module('simpleForum', [
   'ngRoute',
   'simpleForum.index'
---
+++ client/webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack");
+var BowerWebpackPlugin = require("bower-webpack-plugin");
+
+module.exports = {
+    entry : "./app/app.js",
+    output : {
+	path : __dirname,
+	filename : "./app/static/js/bundle.js"
+    },
+    module : {
+	loaders : [ {
+	    test : /\.css$/,
+	    loader : "style!css"
+	}, {
+	    test : /\.(woff|svg|ttf|eot)([\?]?.*)$/,
+	    loader : "file-loader?name=[name].[ext]"
+	} ]
+    },
+    plugins : [ new BowerWebpackPlugin({
+	excludes : /.*\.less/
+    }), new webpack.ProvidePlugin({
+	$ : "jquery",
+	jQuery : "jquery"
+    }) ]
+};
