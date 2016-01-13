95b751d39bc8e13739d80b9b6b9098860f455a61
Martin Bureš <m2bures@gmail.com>
2016-01-13 23:42:58 +0100

    added server/paginator

---
+++ server/utils/Paginator.php
@@ -0,0 +1,8 @@
+<?php
+
+class Paginator {
+	const URL_QUERY_PARAM_PAGE_NUMBER = "pageNumber";
+	const URL_QUERY_PARAM_PAGE_SIZE = "pageSize";
+	const DEFAULT_PAGE = 1;
+	const DEFAULT_PAGE_SIZE = 1000;
+}
