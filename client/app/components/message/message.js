26e183d0188472ef20d7f75fe9193d93c3ac461f
Martin Bureš <m2bures@gmail.com>
2016-01-14 23:45:31 +0100

    client/message component connected to api gw

--- client/app/components/message/message.js
+++ client/app/components/message/message.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const GW_CREATE_MESSAGE_URL = "http://private-c7d92-pwx.apiary-mock.com/messages/";
+const GW_CREATE_MESSAGE_URL = "https://api.sf.sd2.cz/messages/";
 
 angular.module('simpleForum.message', ['ngRoute'])
 
@@ -14,36 +14,39 @@ angular.module('simpleForum.message', ['ngRoute'])
 
 .controller('MessageCreateCtrl', ['$scope','$location','$http','$routeParams','Flash','auth', function($scope,$location,$http,$routeParams,Flash,auth) {
     $scope.create = function(){
-	if(!auth.isAuth()){
-	    $location.path('/login');
-	}
-	
-	let threadId = $location.search()['thread'];
-	let authorId = 1; //auth.getUser.id 
-	
-	$http.post(GW_CREATE_MESSAGE_URL,
-		JSON.stringify({'author':authorId, 'thread':threadId, 'content':$scope.messageContent})
-	).then((res) => {
-	    // success
-	    Flash.create('success', '<strong>Well done!</strong> You successfully created new message.');
-	    $location.replace();
-	    $location.search('thread', null);
-	    $location.path('/thread/' + threadId);
-	}, (res) => {
-	    // fail
-	    Flash.create('warning', '<strong>Oooops!</strong> Some error occurred. Try it again.');
-	    console.log('[FAIL] message create');
-	});
+		if(!auth.isAuth()){
+		    $location.path('/login');
+		}
+		
+		let threadId = $location.search()['thread'];
+		
+		$http.post(GW_CREATE_MESSAGE_URL,
+			JSON.stringify({'threadId':threadId, 'content':$scope.messageContent})
+		).then((res) => {
+		    // success
+			if(res.status == 201){
+				Flash.create('success', '<strong>Well done!</strong> You successfully created new message.');
+			} else {
+				Flash.create('danger', '<strong>Oooops!</strong> Some error occurred. Try it again.');
+			}
+		    $location.replace();
+		    $location.search('thread', null);
+		    $location.path('/thread/' + threadId);
+		}, (res) => {
+		    // fail
+		    Flash.create('warning', '<strong>Oooops!</strong> Some error occurred. Try it again.');
+		    console.log('[FAIL] message create');
+		});
     };
     
     $scope.back = function(){
-	if(!auth.isAuth()){
-	    $location.path('/login');
-	}
-	
-	let threadId = $location.search()['thread'];
-	$location.replace();
-	$location.search('thread', null);
-	$location.path('/thread/' + threadId);
+		if(!auth.isAuth()){
+		    $location.path('/login');
+		}
+		
+		let threadId = $location.search()['thread'];
+		$location.replace();
+		$location.search('thread', null);
+		$location.path('/thread/' + threadId);
     };
 }]);
